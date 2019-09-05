aaf8bd970a75e7ad4259b79865d2bb80d77b3ae9
Alexei Rodriguez <marodrig@alumni.purdue.edu>
2019-09-05 16:24:25 -0400

    Adds tests directory.

--- solution.py
+++ solution.py
@@ -13,7 +13,7 @@ def file_location_user():
     print("Type QUIT to exit.\n")
     file_location = input("Please enter the file name: ")
     if file_location and file_location != 'QUIT':
-        parse_input_file(file_location)
+        return file_location
     elif file_location == 'QUIT':
         exit(1)
     else:
@@ -41,15 +41,21 @@ def create_counter_dict(py_heap):
     py_count_dict = {}
     while py_heap:
         rec = heapq.heappop(py_heap)
-        date_stamp = datetime.datetime.fromtimestamp(rec.epoch).strftime('%m/%d/%Y GMT') 
+        try:
+            date_stamp = datetime.datetime.fromtimestamp(rec.epoch).strftime('%m/%d/%Y GMT') 
+        except (OverflowError, ValueError) as e:
+            print(e)
+            print(e.args)
         if not py_count_dict or date_stamp not in py_count_dict.keys():
             py_count_dict[date_stamp] = {rec.url: 1}
         elif date_stamp in py_count_dict.keys() and rec.url not in py_count_dict[date_stamp].keys():
             py_count_dict[date_stamp][rec.url] = 1 
         elif date_stamp in py_count_dict.keys() and py_count_dict[date_stamp][rec.url]:
             py_count_dict[date_stamp][rec.url] += 1
-
-    nice_print(py_count_dict)
+    if py_count_dict:
+        return py_count_dict
+    else:
+        raise ValueError('Could not build a dictionary.')
 
 
 def nice_print(py_count_dict):
@@ -57,24 +63,27 @@ def nice_print(py_count_dict):
     Print the url/ hit count from the dictionary for each date, in decreasing order of hit count.
     """
     py_lst = []
-    for key, value in py_count_dict.items():
-        # Print date first
-        print(key)
-        # iterate dictionary values for a specific date, and add them to a list.
-        for url, count in value.items():
-            py_lst.append((url, count))
-        # O(N*logN)
-        # Sort the list of url/hit tuples in decreasing order.
-        py_lst.sort(key=lambda tup: tup[1], reverse=True)
-        # - iterate and print each element in the list.
-        for rec in py_lst:
-            print("url: {0} Hit count: {1}".format(rec[0], rec[1]))
-        # empty list for next date.
-        py_lst = []
+    if py_count_dict:
+        for key, value in py_count_dict.items():
+            # Print date first
+            print(key)
+            # iterate dictionary values for a specific date, and add them to a list.
+            for url, count in value.items():
+                py_lst.append((url, count))
+            # O(N*logN)
+            # Sort the list of url/hit tuples in decreasing order.
+            py_lst.sort(key=lambda tup: tup[1], reverse=True)
+            # - iterate and print each element in the list.
+            for rec in py_lst:
+                print("url: {0} Hit count: {1}".format(rec[0], rec[1]))
+            # empty list for next date.
+            py_lst = []
+    else:
+        raise ValueError('Dictionary cannot be None.')
 
 
 try:
-    file_location_user()
+    file_location = file_location_user()
 except IOError as ioe:
     print("Error: {}".format(ioe))
 
---
+++ tests/test_file_import.py
@@ -0,0 +1,11 @@
+import unittest
+
+
+class AnalyticsTestCase(unittest.TestCase):
+    """
+    """
+
+    def test_file_opening(self):
+        """
+        """
+        self.assertEqual("Hi", "Hi")
